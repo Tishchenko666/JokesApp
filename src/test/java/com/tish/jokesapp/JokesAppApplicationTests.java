2dab8ef94713062084b3060668f24ead1aafd94b
Tishchenko666 <s5187565@gmail.com>
2022-07-25 20:57:50 +0200

    Dependency commit

---
+++ src/test/java/com/tish/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tish.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
